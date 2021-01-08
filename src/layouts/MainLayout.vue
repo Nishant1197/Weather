5326789624f86c71d05eadb91bef79c28e520987
Nishant1197 <nishant.chaddha@cilalabs.com>
2021-01-08 11:56:12 +0530

    Weather app completed

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+   
+
+   
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+ data () {
+    return {
+     
+    }
+  }
+}
+</script>
+<style lang="sass">
+.q-page
+   background: linear-gradient(to bottom, #136a8a, #267871)
+
+</style>
